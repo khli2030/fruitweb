2e94dde381bba3c67a09495d468208f2502684d7
khli2030 <054409@gmail.com>
2016-12-08 09:13:12 +0300

    first page

---
+++ fruitProject.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+Name:IBRAHIM BIT TALEB<br/>
+ID:434042905
+<form name="form" method="post" action="try.php">
+
+Email Address:<input type="text" name="email" size="30">
+<input type="submit" name="submit" value="submit">
+
+</body>
+</html>
+
